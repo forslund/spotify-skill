62ca73410e8e2b377b6a20b49d7d92a9f6828fbc
Åke <ake.forslund@gmail.com>
2018-03-01 02:41:12 +0100

    Add requirements.sh script to update librespot (#14)

---
+++ requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+found_exe() {
+    hash "$1" 2>/dev/null
+}
+
+# On a Mark 1 the installation process is often running under a limited
+# user named 'mycroft'.  So avoid apt-get for installing packages.
+
+# polkit uses pkcon instead of apt-get; pkcon will then run apt-get
+if found_exe pkcon; then
+    pkcon install raspotify -y > /dev/null
+fi
+exit 0 # Will fail if package already is on latest version
