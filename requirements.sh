1a885bebba085bc4b9dfb32fdf8e0ac1fe85caf0
Åke Forslund <ake.forslund@gmail.com>
2018-12-11 07:55:17 +0100

    Force yes

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,6 @@
 # Only install raspotify on the Mark-1 which has the correct rights for doing
 # this and the correct repository.
 if grep -q '"platform":.*"mycroft_mark_1"' /etc/mycroft/mycroft.conf; then
-    sudo apt-get install -yq raspotify -o DPkg::Options::=--force-confdef
+    sudo apt-get install --force-yes -yq raspotify -o DPkg::Options::=--force-confdef
 fi
 exit 0
